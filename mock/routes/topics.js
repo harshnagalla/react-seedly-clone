6d03f21f24bab2ce11240adf899d2d5f06605d47
harsh_nagalla <harsh_nagalla@singaporeair.com.sg>
2020-12-14 17:33:26 +0800

    Added new route for topics - search with id

--- mock/routes/topics.js
+++ mock/routes/topics.js
@@ -1,4 +1,7 @@
 const initTopicCollection = require("../db/topics.json");
+const initQuestionCollection = require("../db/questions.json");
+const initUserCollection = require("../db/users.json");
+const initAnswersCollection = require("../db/answers.json");
 
 // lowdb is the json database
 const low = require("lowdb");
@@ -10,7 +13,12 @@ const MemorySync = require("lowdb/adapters/Memory");
 const db = low(new MemorySync());
 
 // initialize the database with data from json file
-db.defaults({ topics: initTopicCollection }).write();
+db.defaults({
+  topics: initTopicCollection,
+  questions: initQuestionCollection,
+  users: initUserCollection,
+  answers: initAnswersCollection,
+}).write();
 // after any interaction that changes the database, use `write()` to commit changes
 
 // create and export plugin
@@ -32,41 +40,67 @@ module.exports = {
        - h is the response toolkit, and it helps with modifying the response (like adding response code)
       */
       handler: (request, h) => {
-        // get all users from users array
+        // get all topics from users array
         const topics = db.get("topics").value();
-        // returning users array will be converted to a json array by hapi
+        // returning topics array will be converted to a json array by hapi
         return topics;
       },
     });
 
-    // /**
-    //  * get single user by id
-    //  */
-    // server.route({
-    //   method: "GET",
-    //   // define path with a required parameter - id
-    //   path: "/user/{id}",
-    //   handler: (request, h) => {
-    //     // get id from request parameters
-    //     const { id } = request.params;
-    //     // find user in array, note that the id needs to be converted to a number, since that's how it's stored in the db
-    //     const user = db
-    //       .get("users")
-    //       .find({ id: parseInt(id, 10) })
-    //       .value();
+    server.route({
+      method: "GET",
+      // define path with a required parameter - id
+      path: "/topic/{id}",
+      handler: (request, h) => {
+        // get id from request parameters
+        const { id } = request.params;
+    
+        const answers = db.get("answers").value();
+        const users = db.get("users").value();
+        const questions = db
+          .get("questions")
+          .filter({ topicId: parseInt(id, 10) })
+          .value();
 
-    //     if (user !== undefined) {
-    //       // uf user is define return
-    //       return user;
-    //     }
-    //     // if user is not found, return an error
-    //     // I'm using the Boom library to generate the errot, this will add the 400 code.
-    //     throw Boom.badRequest(`id ${id} not found`);
-    //     /*
-    //      because you may be matching another API you may need to customize the response.
-    //      you can then use the h toolkit like this: `h.response({error:"BAD ID"}).code(400)`
-    //      */
-    //   },
-    // });
+        const mapAnserwsToUsers = answers.map((answer) => {
+          const user = users.find((item) => {
+            if (item.userId === answer.userId) {
+              return item;
+            }
+          });
+
+          return {
+            userId: user.userId,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            answerId: answer.answerId,
+            answer: answer.answer,
+            timestamp: answer.timestamp,
+            questionId: answer.questionId,
+          };
+        });
+
+        const mapQuestionsToAnswers = questions.map((questionItem) => {
+          const answers = mapAnserwsToUsers.filter((item) => {
+            if (item.questionId === questionItem.questionId) {
+              return item;
+            }
+          });
+
+          const { questionId, question, timestamp } = questionItem;
+          return Object.assign(
+            { question, questionId, timestamp },
+            { answers: answers }
+          );
+        });
+
+        if (mapQuestionsToAnswers !== undefined) {
+          return mapQuestionsToAnswers;
+        }
+        // I'm using the Boom library to generate the error, this will add the 400 code.
+        throw Boom.badRequest(`id ${id} not found`);
+       
+      },
+    });
   },
 };
