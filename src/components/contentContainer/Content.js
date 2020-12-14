31827a68a70eb05ce75806c580d0494a9226206e
harsh_nagalla <harsh_nagalla@singaporeair.com.sg>
2020-12-14 17:33:56 +0800

    Added side navigation

--- src/components/contentContainer/Content.js
+++ src/components/contentContainer/Content.js
@@ -1,59 +1,40 @@
-import Facade from "./facade/Facade";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import style from "./Content.module.scss";
-import UserPost from "./userPost/UserPost";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import QuestionUserPost from "./questionUserPost/QuestionUserPost";
+import SideNavBar from "./sideNavBar/SideNavBar";
+import React, { useEffect, useState } from "react";
+import fetch from "isomorphic-unfetch";
 
-const Content = () => {
+const Content = ({ topics }) => {
+  const [selectedTopicId, setSelectedTopicId] = useState(1);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(async () => {
+    const res = await fetch("http://localhost:7000/topic/" + selectedTopicId, {
+      method: "GET",
+    });
+    const json = await res.json();
+    setQuestions(json);
+  }, [selectedTopicId]);
+
+  const getQuestionsforTopic = async (topicId) => {
+    setSelectedTopicId(topicId);
+  };
+
   return (
-    <Grid container spacing={3} className={style.content}>
-      <Grid item xs={3}>
-        <div className={style.content__allContentTile}>
-          <label>{"All Content"}</label>
-        </div>
+    <div className={style.content}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <SideNavBar
+            topics={topics}
+            getQuestionsforTopic={getQuestionsforTopic}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <QuestionUserPost questions={questions} />
+        </Grid>
       </Grid>
-      <Grid item xs={8}>
-        <UserPost />
-      </Grid>
-      <Grid item xs={3}>
-        <Card className={style.root}>
-          <CardContent>
-            <Typography
-              className={style.content_myTopicsTitle}
-              color="textSecondary"
-              gutterBottom
-            >
-              Word of the Day
-            </Typography>
-            <Typography color="textSecondary">
-              adjective
-            </Typography>
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={3}>
-        <Paper className={style.paper}>xs=3</Paper>
-      </Grid>
-      <Grid item xs={3}>
-        <Paper className={style.paper}>xs=3</Paper>
-      </Grid>
-      <Grid item xs={3}>
-        <Paper className={style.paper}>xs=3</Paper>
-      </Grid>
-    </Grid>
+    </div>
   );
 };
 export default Content;
--- src/components/contentContainer/sideNavBar/SideNavBar.js
+++ src/components/contentContainer/sideNavBar/SideNavBar.js
@@ -1,3 +1,37 @@
-const SideNavBar = () => {
-    return()
-}
+import React, { useCallback } from "react";
+import style from "./SideNavBar.module.scss";
+
+const SideNavBar = ({ topics, getQuestionsforTopic }) => {
+  const onItemClick = useCallback(
+    (topicId) => {
+      getQuestionsforTopic(topicId);
+    },
+    [topics]
+  );
+  const renderTopics = () => {
+    return topics.map((topic) => {
+      return (
+        <li
+          onClick={() => onItemClick(topic.topicId)}
+          className={style.sideNavBar__topics}
+          key={topic.topicId}
+        >
+          {topic.topicName}
+        </li>
+      );
+    });
+  };
+  return (
+    <>
+      <div className={style.sideNavBar__allContentTile}>
+        <label className={style.sideNavBar__allContentTileLabel}>{"All Content"}</label>
+      </div>
+      <ul className={style.sideNavBar}>
+        <li className={style.sideNavBar__featuredTopic}>{"Featured Topics"}</li>
+        {renderTopics()}
+      </ul>
+    </>
+  );
+};
+
+export default SideNavBar;
